1534fe1a376884d5a8ea30a934c58ae9758cd1df
Bosnich Timothy Bonsaleng <bosnichtimothy4@gmail.com>
2023-03-16 10:24:55 +0700

    hello world

--- pemvisualisasiRTH/resources/views/homePage.php
+++ pemvisualisasiRTH/resources/views/homePage.php
@@ -2,12 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
 </head>
 <body>
     <h1>Hellow World!</h1>
-    <p>Selamat datang di dunia tipu-tipu</p>
 </body>
 </html>
